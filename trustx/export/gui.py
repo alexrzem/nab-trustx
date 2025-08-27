a7827bae09b7952ca8bbb64bd7e43aee31c9a158
Alex <alex@rzem.com>
2025-08-27 14:11:25 +1000

    updates

--- trustx/export/gui.py
+++ trustx/export/gui.py
@@ -1,35 +1,58 @@
+import json
+from typing import Dict, List, Optional, Union
 from textual import events, on
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Select, Static, RichLog, Input
-from textual.widgets._select import NoSelection
+from textual.containers import Horizontal, HorizontalGroup, HorizontalScroll, Vertical, VerticalGroup, VerticalScroll
+from textual.widgets import Button, Footer, Header, Select, Static, RichLog, Input, SelectionList
+from textual.widgets.selection_list import Selection
 
-
-BLANK = NoSelection()
-
-ENVIRONMENTS = [("Dev", "DEV"), ("Sit", "SIT"), ("Prd", "PROD")]
+CONFIG = []
+ENVIRONMENTS = []
 ENVIRONMENT = None
 
 
-class EnvironmentSelector(Select):
+def load_config() -> List:
+    with open("config.json") as json_data:
+        CONFIG = json.load(json_data)
+
+    for item in CONFIG:
+        ENVIRONMENTS.append((item["name"], item["id"]))
+
+    return CONFIG
+
+
+def get_environment(environment_id: str) -> Dict | None:
+    environments = load_config()
+    selected = environments[0]
+    for environment in environments:
+        if environment["id"] == environment_id:
+            selected = environment
+            break
+
+    return selected
+
+
+class EnvironmentSelector(Static):
+
+    def compose(self) -> ComposeResult:
+        yield Select(options=ENVIRONMENTS, id="environments", name="environments", allow_blank=True, prompt="Select Environment")
+
+
+class EnvironmentHost(Input):
+
+    BORDER_TITLE = "Host Name"
+
     def __init__(self) -> None:
-        super().__init__(options=ENVIRONMENTS, id="environments", name="environments", allow_blank=False, prompt="Select Environment")
-
-
-class EnvironmentDomain(Input):
-
-    BORDER_TITLE = "Domain Name"
-
-    def __init__(self) -> None:
-        super().__init__(id="domain", placeholder="Domain", password=False)
+        super().__init__(id="host", placeholder="host", password=False)
         self.classes = "environment-input"
 
 
-class EnvironmentUsername(Input):
+class EnvironmentName(Input):
 
-    BORDER_TITLE = "Username"
+    BORDER_TITLE = "Name"
 
     def __init__(self) -> None:
-        super().__init__(id="username", placeholder="Username", password=False)
+        super().__init__(id="name", placeholder="Name", password=False)
         self.classes = "environment-input"
 
 
@@ -46,17 +69,14 @@ class EnvironmentBox(Static):
     """Display a greeting."""
 
     BORDER_TITLE = "Environments"
-    CSS = """
-
-    """
 
     def __init__(self) -> None:
         super().__init__(id="environment-box")
 
     def compose(self) -> ComposeResult:
         yield EnvironmentSelector()
-        yield EnvironmentDomain()
-        yield EnvironmentUsername()
+        yield EnvironmentHost()
+        yield EnvironmentName()
         yield EnvironmentApikey()
 
 
@@ -64,16 +84,19 @@ class SettingsBox(Static):
     """Display a greeting."""
 
     BORDER_TITLE = "Settings"
-    CSS = """
-    #settings-box {
-        height: 100%;
-        width: 1fr;
-        border: solid #c678dd;
-    }
-    """
 
     def __init__(self) -> None:
         super().__init__(id="settings-box")
+
+    def compose(self) -> ComposeResult:
+        yield SelectionList[str](
+            Selection("Cloud Functions", "Cloud Functions", False),
+            Selection("Custom Data Forms", "Custom Data Forms", False),
+            Selection("Custom Pages", "Custom Pages", False),
+            Selection("Process Definitions", "Process Definitions", False),
+            Selection("Themes", "Themes", False),
+        )
+        yield Button("Export", variant="primary")
 
     def action_next_word(self) -> None:
         """Get a new hello and update the content area."""
@@ -84,9 +107,6 @@ class LogsBox(Static):
     """Display a greeting."""
 
     BORDER_TITLE = "Logs"
-    DEFAULT_CSS = """
-
-    """
 
     def __init__(self) -> None:
         super().__init__(id="logging-box")
@@ -106,6 +126,10 @@ class ExportApp(App):
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
     CSS_PATH = "style.tcss"
 
+    def __init__(self) -> None:
+        super().__init__()
+        load_config()
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
@@ -122,21 +146,27 @@ class ExportApp(App):
 
     def on_ready(self) -> None:
         """Called  when the DOM is ready."""
-        text_log = self.query_one("#log", RichLog)
-        text_log.write("[bold magenta]Write text or any Rich renderable!")
+
+        TEXT_LOG = self.query_one("#log", RichLog)
+        TEXT_LOG.write("[bold magenta]Write text or any Rich renderable!")
 
     def on_key(self, event: events.Key) -> None:
         """Write Key events to log."""
-        text_log = self.query_one("#log", RichLog)
-        text_log.write(event)
+        TEXT_LOG = self.query_one("#log", RichLog)
+        TEXT_LOG.write(event)
 
     @on(Select.Changed)
     def select_changed(self, event: Select.Changed) -> None:
         """Write Key events to log."""
-        text_log = self.query_one("#log", RichLog)
-        text_log.write(event.value)
+        TEXT_LOG = self.query_one("#log", RichLog)
+        TEXT_LOG.write(event.value)
 
-        text_log = self.query()
+        environment = get_environment(str(event.value))
+        TEXT_LOG.write(environment)
+        if environment:
+            self.query_one("#host", Input).value = environment["host"]
+            self.query_one("#name", Input).value = environment["name"]
+            self.query_one("#apikey", Input).value = environment["apikey"]
 
 
 if __name__ == "__main__":
